f3623f9d3f6207fccb875dac38da7eae330f129c
Vajith Chamuditha <vajithc@gmail.com>
2021-04-15 19:35:15 +0530

    added styles js files

---
+++ js/myScript.js
@@ -0,0 +1,102 @@
+function loadData(name){	
+	if(name==button1){
+	document.getElementById("img1").src ="images/phone 1.jpg";
+	document.getElementById("para").innerHTML = "**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>**********Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS Iphone XS ***********<br>"
+	}
+	else if (name==button2){
+	document.getElementById("img1").src ="images/phone 2.jpg";
+	document.getElementById("para").innerHTML = "***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>***********Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max Iphone XS Max ***********<br>"
+	}
+	else if(name==button3){
+	document.getElementById("img1").src ="images/phone 3.jpg";
+	document.getElementById("para").innerHTML = "***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>***********Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus Iphone 8s plus ***********<br>"
+	}
+	else if(name==button4){
+	document.getElementById("img1").src ="images/phone 4.jpg";
+	document.getElementById("para").innerHTML = "**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>**********Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone SE Iphone XS Iphone SE Iphone SE Iphone XS Iphone SE **********<br>"
+	}
+}
+
+function priceForLoop(){
+	var product =["Iphone XS =$1,349","Iphone XS =$1,349","Iphone XS =$999","Iphone 8 plus =$599","Iphone SE =$399"];
+	var i ;
+	var length= product.length;
+	var txt= " ";
+	for(i=0; i<length; i++){
+	txt += product[i] + "<br/>";
+	}
+	document.getElementById("img1").src ="images/phone.jpg";
+	document.getElementById("para").innerHTML =txt;
+}
+
+function productForInLoop(){
+	var product =["Iphone XS - $1,349","Iphone XS Max - $1,449","Iphone XS Max Pro  - $999","Iphone 8 plus - $599","Iphone SE - $399"];
+	var i;
+	var length= product.length;
+	var txt= "";
+	for(i in product){
+	txt += product[i] + "<br/>";
+	}
+	document.getElementById("img1").src ="images/phone.jpg";
+	document.getElementById("para").innerHTML =txt;
+}
+
+
+function priceHigher(){
+	var product=[];
+	var txt = "";
+	
+	product["Iphone XS"] = 1349;
+	product["Iphone XS Max"] = 1449;
+	product["Iphone XS Max Pro"] = 999;
+	product["Iphone 8 plus"] = 599;
+	product["Iphone SE"] = 399;
+	
+	for(var item in product){
+		if (product[item]>1000){
+			txt +=item + " : "+"$" + product[item] +"<br>";
+		}
+	}
+	document.getElementById("img1").src ="images/phone.jpg";
+	document.getElementById("para").innerHTML =txt;
+}
+
+
+function priceLower(){
+	var product=[];
+	var txt = "";
+	
+	product["Iphone XS"] = 1349;
+	product["Iphone XS Max"] = 1449;
+	product["Iphone XS Max Pro"] = 999;
+	product["Iphone 8 plus"] = 599;
+	product["Iphone SE"] = 399;
+	
+	for(var item in product){
+		if (product[item]<1000){
+			txt +=item + " : "+"$" + product[item] +"<br>";
+		}
+	}
+	document.getElementById("img1").src ="images/phone.jpg";
+	document.getElementById("para").innerHTML=txt;
+
+}
+
+function checkPassword(){
+	if(document.getElementById("pwd").value != document.getElementById("rpwd").value){
+		alert("Password Mismatch !")
+		return false;
+	}
+	else{
+		alert("Success !")
+		return true;
+	}
+}
+function enableButton(){
+	if(document.getElementById("policy").checked){
+		document.getElementById("btn1").disabled=false;
+	}
+	else{
+		document.getElementById("btn1").disabled=true;
+	}
+}
